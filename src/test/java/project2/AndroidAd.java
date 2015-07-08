8f97e4d3c60f572f4fefe3e521a0d88088b6a270
Stefania Delprete <ste.stunning@gmail.com>
2015-07-08 17:59:46 +0200

    Project2 update
    Finished the first task

---
+++ src/test/java/project2/AndroidAd.java
@@ -0,0 +1,20 @@
+package project2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AndroidAd {
+
+    public static void main(String[] args){
+
+        // declare variables
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = "http://www.goeuro.com";
+
+        // open Firefox and redirect to goeuro.com
+        driver.get(baseUrl);
+
+        // click on the Android ads
+        
+    }
+}
--- src/test/java/project2/TypeCities.java
+++ src/test/java/project2/TypeCities.java
@@ -1,16 +1,25 @@
 package project2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-/**
- * Created by stefania on 08/07/15.
- */
 public class TypeCities {
     public static void main(String[] args) {
         // declaration and installation
         WebDriver driver = new FirefoxDriver();
         String baseUrl = "http://www.goeuro.com";
+
+        // launch Firefox and direct to the base URL
+        driver.get(baseUrl);
+
+        // Find the element of the departure city by xPath and type a value
+        driver.findElement(By.xpath(".//*[@id='from_filter']")).clear();
+        driver.findElement(By.xpath(".//*[@id='from_filter']")).sendKeys("Barcelona");
+
+        // Type a value in the arrival city
+        driver.findElement(By.xpath(".//*[@id='to_filter']")).clear();
+        driver.findElement(By.xpath(".//*[@id='to_filter']")).sendKeys("London");
 
     }
 }
